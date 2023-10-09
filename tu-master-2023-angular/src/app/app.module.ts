87acb3d017ffc6ae3eba3f5ddf63bd3ba5ff677f
p.bodlev <pyotr.bodlev@dss.bg>
2023-10-09 21:39:54 +0300

    create login page base functionality

--- tu-master-2023-angular/src/app/app.module.ts
+++ tu-master-2023-angular/src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
 import { HomeComponent } from './core/components/home/home.component';
 import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, StyleClassModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- tu-master-2023-angular/src/app/auth/components/login/login.component.ts
+++ tu-master-2023-angular/src/app/auth/components/login/login.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { StyleClassModule } from 'primeng/styleclass';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    StyleClassModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private authService: AuthService) {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  submitForm() {
+    let { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password);
+  }
 }
